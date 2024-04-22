19317694fbbf8c6f6b9d5a463e075e7850803802
Lea Antony <tonimemangkeren76@gmail.com>
2024-04-22 19:25:21 +0700

    Update bagian pengaturan akun admin

---
+++ public/js/template/pengaturan_akun.js
@@ -0,0 +1,25 @@
+function showSection(section) {
+    // Semua bagian diubah menjadi display: none
+    document.getElementById('identitasSection').classList.add('hidden');
+    document.getElementById('fotoProfilSection').classList.add('hidden');
+    document.getElementById('kataSandiSection').classList.add('hidden');
+
+    // Tombol yang sedang aktif diberikan kelas 'bg-blue-600'
+    document.getElementById('identitasBtn').classList.remove('bg-gradient-to-r', 'from-green-400', 'to-teal-400',
+        'hover:from-teal-300', 'hover:to-green-300', 'active:from-teal-200', 'active:to-green-200',
+        'focus:outline-none', 'focus:ring', 'focus:ring-emerald-300', 'text-white');
+    document.getElementById('fotoProfilBtn').classList.remove('bg-gradient-to-r', 'from-green-400', 'to-teal-400',
+        'hover:from-teal-300', 'hover:to-green-300', 'active:from-teal-200', 'active:to-green-200',
+        'focus:outline-none', 'focus:ring', 'focus:ring-emerald-300', 'text-white');
+    document.getElementById('kataSandiBtn').classList.remove('bg-gradient-to-r', 'from-green-400', 'to-teal-400',
+        'hover:from-teal-300', 'hover:to-green-300', 'active:from-teal-200', 'active:to-green-200',
+        'focus:outline-none', 'focus:ring', 'focus:ring-emerald-300', 'text-white');
+
+    // Bagian yang dipilih ditampilkan
+    document.getElementById(section + 'Section').classList.remove('hidden');
+
+    // Tombol yang sedang aktif diberikan warna latar belakang
+    document.getElementById(section + 'Btn').classList.add('bg-gradient-to-r', 'from-green-400', 'to-teal-400',
+        'hover:from-teal-300', 'hover:to-green-300', 'active:from-teal-200', 'active:to-green-200',
+        'focus:outline-none', 'focus:ring', 'focus:ring-emerald-300', 'text-white');
+}
--- resources/views/admin/pengaturan_akun.blade.php
+++ resources/views/admin/pengaturan_akun.blade.php
@@ -9,87 +9,98 @@
 <div class="text-center">
     <div class="card bg-base-100 shadow-xl mx-16 overflow-visible -mt-10">
         <div class="card-body">
-            <div class="mb-20">
-                <h2 class="card-title mb-10">Identitas</h2>
-                <form action="/admin/usaha">
-                    <div class="grid grid-rows-2 grid-flow-col gap-3">
-                        <label class="form-control w-full mb-10">
-                            <div class="label">
-                                <span class="label-text">Nama</span>
+            <div>
+                <h2 class="card-title mb-3">
+                    <button
+                        class="btn sm:btn-sm md:btn-md lg:btn-lg bg-gradient-to-r from-green-400 to-teal-400 hover:from-teal-300 hover:to-green-300 active:from-teal-200 active:to-green-200 focus:outline-none focus:ring focus:ring-emerald-300 text-white"
+                        onclick="showSection('identitas')" id="identitasBtn">Identitas</button>
+                    <button class="btn sm:btn-sm md:btn-md lg:btn-lg" onclick="showSection('fotoProfil')"
+                        id="fotoProfilBtn">Foto Profil</button>
+                    <button class="btn sm:btn-sm md:btn-md lg:btn-lg" onclick="showSection('kataSandi')"
+                        id="kataSandiBtn">Kata Sandi</button>
+                </h2>
+                <div id="identitasSection">
+                    <form action="/admin/usaha">
+                        <div class="grid grid-rows-2 grid-flow-col gap-3">
+                            <label class="form-control w-full mb-10">
+                                <div class="label">
+                                    <span class="label-text">Nama</span>
+                                </div>
+                                <input type="text" placeholder="Nama Admin" class="input input-accent w-full" />
+                            </label>
+                            <label class="form-control w-full">
+                                <div class="label">
+                                    <span class="label-text">Email</span>
+                                </div>
+                                <input type="text" placeholder="Email Admin" class="input input-accent w-full" />
+                            </label>
+                            <label class="form-control w-full">
+                                <div class="label">
+                                    <span class="label-text">Username</span>
+                                </div>
+                                <input type="text" placeholder="Username Admin" class="input input-accent w-full" />
+                            </label>
+                            <label class="form-control w-full">
+                                <div class="label">
+                                    <span class="label-text">No HP</span>
+                                </div>
+                                <input type="text" placeholder="No HP Admin" class="input input-accent w-full" />
+                            </label>
+                        </div>
+                        <x-button class="sm:btn-sm md:btn-md lg:btn-lg mt-5">Edit</x-button>
+                        <x-button class="sm:btn-sm md:btn-md lg:btn-lg mt-5">Simpan</x-button>
+                    </form>
+                </div>
+                <div id="fotoProfilSection" class="hidden">
+                    <form action="/admin/usaha">
+                        <div class="avatar">
+                            <div class="w-48 rounded-full border">
+                                <img src="https://cdn-icons-png.flaticon.com/512/21/21104.png" alt="profile"
+                                    class="shadow rounded-full object-cover" />
                             </div>
-                            <input type="text" placeholder="Nama Admin" class="input input-accent w-full" />
-                        </label>
+                        </div>
                         <label class="form-control w-full">
                             <div class="label">
-                                <span class="label-text">Email</span>
+                                <span class="label-text">File Foto</span>
                             </div>
-                            <input type="text" placeholder="Email Admin" class="input input-accent w-full" />
+                            <input type="file" class="file-input file-input-bordered file-input-accent w-full" />
                         </label>
-                        <label class="form-control w-full">
+                        <x-button class="sm:btn-sm md:btn-md lg:btn-lg mt-5">Edit</x-button>
+                        <x-button class="sm:btn-sm md:btn-md lg:btn-lg mt-5">Simpan</x-button>
+                    </form>
+                </div>
+                <div id="kataSandiSection" class="hidden">
+                    <form action="/admin/usaha">
+                        <label class="form-control w-full mb-5">
                             <div class="label">
-                                <span class="label-text">Username</span>
+                                <span class="label-text">Kata Sandi Saat ini</span>
                             </div>
-                            <input type="text" placeholder="Username Admin" class="input input-accent w-full" />
+                            <input type="text" placeholder="Masukkan Kata Sandi Saat ini"
+                                class="input input-accent w-full" />
                         </label>
-                        <label class="form-control w-full">
+                        <label class="form-control w-full mb-5">
                             <div class="label">
-                                <span class="label-text">No HP</span>
+                                <span class="label-text">Kata Sandi Baru</span>
                             </div>
-                            <input type="text" placeholder="No HP Admin" class="input input-accent w-full" />
+                            <input type="text" placeholder="Masukkan Kata Sandi Baru"
+                                class="input input-accent w-full" />
                         </label>
-                    </div>
-                    <x-button class="sm:btn-sm md:btn-md lg:btn-lg mt-5">Edit</x-button>
-                    <x-button class="sm:btn-sm md:btn-md lg:btn-lg mt-5">Simpan</x-button>
-                </form>
-            </div>
-            <hr class="my-5">
-            <div class="mb-20">
-                <h2 class="card-title mb-10">Foto</h2>
-                <form action="/admin/usaha">
-                    <div class="avatar">
-                        <div class="w-48 rounded-full border">
-                            <img src="https://cdn-icons-png.flaticon.com/512/21/21104.png" alt="profile"
-                            class="shadow rounded-full object-cover" />
-                        </div>
-                    </div>
-                    <label class="form-control w-full">
-                        <div class="label">
-                            <span class="label-text">Nama</span>
-                        </div>
-                        <input type="file" class="file-input file-input-bordered file-input-accent w-full" />
-                    </label>
-                    <x-button class="sm:btn-sm md:btn-md lg:btn-lg mt-5">Edit</x-button>
-                    <x-button class="sm:btn-sm md:btn-md lg:btn-lg mt-5">Simpan</x-button>
-                </form>
-            </div>
-            <hr class="my-5">
-            <div class="mb-20">
-                <h2 class="card-title mb-10">Kata Sandi</h2>
-                <form action="/admin/usaha">
-                    <label class="form-control w-full mb-10">
-                        <div class="label">
-                            <span class="label-text">Kata Sandi Saat ini</span>
-                        </div>
-                        <input type="text" placeholder="Masukkan Kata Sandi Saat ini" class="input input-accent w-full" />
-                    </label>
-                    <label class="form-control w-full mb-10">
-                        <div class="label">
-                            <span class="label-text">Kata Sandi Baru</span>
-                        </div>
-                        <input type="text" placeholder="Masukkan Kata Sandi Baru" class="input input-accent w-full" />
-                    </label>
-                    <label class="form-control w-full mb-10">
-                        <div class="label">
-                            <span class="label-text">Konfirmasi Kata Sandi Baru</span>
-                        </div>
-                        <input type="text" placeholder="Masukkan Konfirmasi Kata Sandi Baru" class="input input-accent w-full" />
-                    </label>
-                    <x-button class="sm:btn-sm md:btn-md lg:btn-lg mt-5">Edit</x-button>
-                    <x-button class="sm:btn-sm md:btn-md lg:btn-lg mt-5">Simpan</x-button>
-                </form>
+                        <label class="form-control w-full mb-5">
+                            <div class="label">
+                                <span class="label-text">Konfirmasi Kata Sandi Baru</span>
+                            </div>
+                            <input type="text" placeholder="Masukkan Konfirmasi Kata Sandi Baru"
+                                class="input input-accent w-full" />
+                        </label>
+                        <x-button class="sm:btn-sm md:btn-md lg:btn-lg">Edit</x-button>
+                        <x-button class="sm:btn-sm md:btn-md lg:btn-lg">Simpan</x-button>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
 </div>
+
+<script src="/js/template/pengaturan_akun.js"></script>
 
 @endsection
